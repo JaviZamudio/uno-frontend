af6b2821f15af1efc2c2cd3057a4213bbcbe33cd
JaviZamudio <javier.elihu100@gmail.com>
2022-10-20 11:40:40 -0500

    Color selection dialog for wildcard complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,71 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import ColorModal from './ColorModal';
 
-const Card = ({ data, disabled, onClick: handleClick, width }) => {
-  const colorModalRef = useRef(null);
-  let cardName = data.type === "number" || data.type === "action" ? data.color[0].toUpperCase() + "_" + data.value[0].toUpperCase() + ".png" : (data.value === "wild" ? "W.png" : "W+4.png");
+// const Card = ({ data, disabled, onClick: handleClick, width }) => {
+//   const colorModalRef = useRef(null);
+//   let cardName = data.type === "number" || data.type === "action" ? data.color[0].toUpperCase() + "_" + data.value[0].toUpperCase() + ".png" : (data.value === "wild" ? "W.png" : "W+4.png");
 
-  if (data.type === "deck") {
-    cardName = "UNO_back.png";
-  }
+//   if (data.type === "deck") {
+//     cardName = "UNO_back.png";
+//   }
 
-  const handleWildClick = () => {
-    colorModalRef.current.style.display = "block";
-  }
+//   const handleWildClick = () => {
+//     colorModalRef.current.style.display = "block";
+//   }
 
-  const handleColor = (color) => {
-    colorModalRef.current.style.display = "none";
-    data.color = color;
-    handleClick(data);
-  }
+//   const handleColor = (color) => {
+//     colorModalRef.current.style.display = "none";
+//     data.color = color;
+//     handleClick(data);
+//   }
 
-  return (
-    <div className={"card" + (disabled ? " disabled" : "")} onClick={disabled ? null : (data.type === "wild" ? handleWildClick : () => handleClick && handleClick(data))} >
-      { // color selection modal
-        data.type === "wild" &&
-        <ColorModal onClick={handleColor} compRef={colorModalRef} />
-      }
+//   return (
+//     <div className={"card" + (disabled ? " disabled" : "")} onClick={disabled ? null : (data.type === "wild" ? handleWildClick : () => handleClick && handleClick(data))} >
+//       { // color selection modal
+//         data.type === "wild" &&
+//         <ColorModal onClick={handleColor} compRef={colorModalRef} />
+//       }
 
-      <img src={require(`../images/cards/${cardName}`)} alt={cardName} style={{ width: width || "12rem" }}/>
-    </div>
-  )
+//       <img src={require(`../images/cards/${cardName}`)} alt={cardName} style={{ width: width || "12rem" }}/>
+//     </div>
+//   )
+// }
+
+function Card({ data: card, disabled, onClick, width }) {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    // Get the card name
+    const cardName = card.type === "deck" ?
+        "UNO_back.png" :
+        card.type === "wild" ?
+            (card.value === "wild" ? "W.png" : "W+4.png") :
+            card.color[0].toUpperCase() + "_" + card.value[0].toUpperCase() + ".png";
+
+    const handleClick = (card) => {
+        if (card.type === "wild") {
+            setModalVisible(true);
+        } else {
+            onClick(card);
+        }
+    }
+
+    const handleColorSelection = (color) => {
+        setModalVisible(false);
+        onClick({ ...card, color });
+    }
+
+    return (
+        <>
+            <ColorModal visible={modalVisible} onClose={() => setModalVisible(false)} onColorSelection={handleColorSelection} />
+
+            <div
+                className={"card" + (disabled ? " disabled" : "")}
+                onClick={disabled ? null : () => { handleClick(card) }}
+            >
+                <img src={require(`../images/cards/${cardName}`)} alt={cardName} style={{ width: width || "12rem" }} />
+            </div>
+        </>
+    );
 }
 
-export default Card
-
-
+export default Card;
--- src/components/ColorModal.jsx
+++ src/components/ColorModal.jsx
@@ -1,12 +1,64 @@
-function ColorModal({ onClick, compRef }) {
-    return <div className="color-Modal" ref={compRef} style={{ display: "none" }}>
-        <div className="colors">
-            <button className="color-red" onClick={() => onClick("red")}>RED</button>
-            <button className="color-blue" onClick={() => onClick("blue")}>BLUE</button>
-            <button className="color-green" onClick={() => onClick("green")}>GREEN</button>
-            <button className="color-yellow" onClick={() => onClick("yellow")}>YELLOW</button>
-        </div>
-    </div>;
+import { Button, ButtonGroup, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { useState } from "react";
+
+function ColorModal({ visible: modalVisible, onClose, onColorSelection }) {
+    const [color, setColor] = useState("");
+
+    const handleClose = () => {
+        onClose();
+        setColor("");
+    }
+
+    /**
+     * Function to handle color selection and submit of the modal
+     * @param {string} color 
+     */
+    const handleColorSelection = (color) => {
+        onColorSelection(color);
+        handleClose();
+    }
+
+    const handleColorButtonClick = (color) => {
+        setColor(color);
+    }
+
+    return (
+        <Dialog open={modalVisible} onClose={handleClose}>
+            <DialogTitle>Choose a color</DialogTitle>
+            <DialogContent>
+                <ButtonGroup variant="outlined" color="inherit">
+                    <Button
+                        onClick={() => handleColorButtonClick("red")}
+                        style={color === "red" ? { backgroundColor: "red", color: "white", borderColor: "black" } : {}}
+                    >
+                        Red
+                    </Button>
+                    <Button
+                        onClick={() => handleColorButtonClick("blue")}
+                        style={color === "blue" ? { backgroundColor: "blue", color: "white", borderColor: "black" } : {}}
+                    >
+                        Blue
+                    </Button>
+                    <Button
+                        onClick={() => handleColorButtonClick("green")}
+                        style={color === "green" ? { backgroundColor: "green", color: "white", borderColor: "black" } : {}}
+                    >
+                        Green
+                    </Button>
+                    <Button
+                        onClick={() => handleColorButtonClick("yellow")}
+                        style={color === "yellow" ? { backgroundColor: "yellow", color: "black", borderColor: "black" } : {}}
+                    >
+                        Yellow
+                    </Button>
+                </ButtonGroup>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={() => handleColorSelection(color)} disabled={!color}>Accept</Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
 
 export default ColorModal;
